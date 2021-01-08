20eaac04b3d41eba853d82e3e974c046f7e38c0c
rizqonmaulana <rizqonmaulana5@gmail.com>
2021-01-08 21:44:09 +0800

    fix : toUpperCase & toLowerCase warning

--- src/components/_base/productDetail/ProductDeliveryAndTime.vue
+++ src/components/_base/productDetail/ProductDeliveryAndTime.vue
@@ -1,85 +1,107 @@
 <template>
-    <div>
-        <div class="product-name">
-            <p>
-                <span style="color: #4F5665;">Favorite & Promo </span> <span style="color: #6a4029;"><strong> > {{productName.toLowerCase()}} </strong></span>
-            </p>
-        </div>
-        <div class="product-pic text-center">
-            <img src="../../../assets/cold_brew.png" class="rounded-circle mt-5 mb-4 mx-auto" style="width: 250px;">
-        </div>
-        <div class="product-card mt-5">
-            <div class="delivery-time-card">
-                <h5>Delivery and Time</h5>
-                <div class="delivery-btn-collection-1 mt-4">
-                    <button 
-                        v-if="deliveryDineIn != 0" 
-                        @click="selectDelivery('Dine In')" 
-                        :class="btnDelivery === 'Dine In' ? 'delivery-btn active' : 'delivery-btn' "
-                        >
-                            Dine In
-                        </button>
-                    <button 
-                        v-if="deliveryHome != 0" 
-                        @click="selectDelivery('Door Delivery')" 
-                        :class="btnDelivery === 'Door Delivery' ? 'delivery-btn active' : 'delivery-btn'"
-                        >
-                            Door Deliver
-                        </button>
-                    <button 
-                        v-if="deliveryTakeAway != 0" 
-                        @click="selectDelivery('Pick Up')" 
-                        :class="btnDelivery === 'Pick Up' ? 'delivery-btn active' : 'delivery-btn'">
-                            Pick up
-                        </button>
-                </div>
-                    <br>
-                    <span class="mr-5">Now</span>
-                    <button 
-                        @click="selectNow('yes')"
-                        :class="btnNow === 'yes' ? 'delivery-btn active mr-2' : 'delivery-btn mr-2'"
-                        >
-                            Yes
-                        </button>
-                    <button 
-                        @click="selectNow('no')"
-                         :class="btnNow === 'no' ? 'delivery-btn active' : 'delivery-btn'"
-                        >
-                            No
-                    </button>
-                    <br>
-                <div class="set-delivery-time mt-4 mb-4">
-                    <span class="mr-4">Set time</span>
-                    <input type="text" class="poppins" placeholder="Enter time for reservation">
-                </div>
-            </div>
-        </div>
+  <div>
+    <div class="product-name">
+      <p>
+        <span style="color: #4F5665;">Favorite & Promo </span>
+        <span style="color: #6a4029;"
+          ><strong class="text-lowercase"> > {{ productName }} </strong></span
+        >
+      </p>
     </div>
+    <div class="product-pic text-center">
+      <img
+        src="../../../assets/cold_brew.png"
+        class="rounded-circle mt-5 mb-4 mx-auto"
+        style="width: 250px;"
+      />
+    </div>
+    <div class="product-card mt-5">
+      <div class="delivery-time-card">
+        <h5>Delivery and Time</h5>
+        <div class="delivery-btn-collection-1 mt-4">
+          <button
+            v-if="deliveryDineIn != 0"
+            @click="selectDelivery('Dine In')"
+            :class="
+              btnDelivery === 'Dine In' ? 'delivery-btn active' : 'delivery-btn'
+            "
+          >
+            Dine In
+          </button>
+          <button
+            v-if="deliveryHome != 0"
+            @click="selectDelivery('Door Delivery')"
+            :class="
+              btnDelivery === 'Door Delivery'
+                ? 'delivery-btn active'
+                : 'delivery-btn'
+            "
+          >
+            Door Deliver
+          </button>
+          <button
+            v-if="deliveryTakeAway != 0"
+            @click="selectDelivery('Pick Up')"
+            :class="
+              btnDelivery === 'Pick Up' ? 'delivery-btn active' : 'delivery-btn'
+            "
+          >
+            Pick up
+          </button>
+        </div>
+        <br />
+        <span class="mr-5">Now</span>
+        <button
+          @click="selectNow('yes')"
+          :class="
+            btnNow === 'yes' ? 'delivery-btn active mr-2' : 'delivery-btn mr-2'
+          "
+        >
+          Yes
+        </button>
+        <button
+          @click="selectNow('no')"
+          :class="btnNow === 'no' ? 'delivery-btn active' : 'delivery-btn'"
+        >
+          No
+        </button>
+        <br />
+        <div class="set-delivery-time mt-4 mb-4">
+          <span class="mr-4">Set time</span>
+          <input
+            type="text"
+            class="poppins"
+            placeholder="Enter time for reservation"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   props: [
-          'productName',
-          'productPic',
-          'deliveryDineIn',
-          'deliveryHome',
-          'deliveryTakeAway'
+    'productName',
+    'productPic',
+    'deliveryDineIn',
+    'deliveryHome',
+    'deliveryTakeAway'
   ],
   data() {
-      return {
-          btnDelivery: '',
-          btnNow: ''
-      }
+    return {
+      btnDelivery: '',
+      btnNow: ''
+    }
   },
   methods: {
-      selectDelivery(delivery) {
-          this.btnDelivery = delivery
-          this.$emit('selectDelivery', delivery)
-      },
-      selectNow(now) {
-         this.btnNow = now
-      }
+    selectDelivery(delivery) {
+      this.btnDelivery = delivery
+      this.$emit('selectDelivery', delivery)
+    },
+    selectNow(now) {
+      this.btnNow = now
+    }
   }
 }
 </script>
@@ -89,7 +111,7 @@ export default {
   background-color: #fff;
   border-radius: 20px;
   -webkit-box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.2);
-          box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.2);
   padding: 27px 30px;
 }
 
@@ -103,7 +125,7 @@ export default {
   padding: 3px 15px;
   margin-right: 5px;
   border: 1px solid rgba(186, 186, 186, 0.35);
-  outline:none;
+  outline: none;
 }
 
 .product-card .delivery-btn.active,
@@ -111,7 +133,7 @@ export default {
   color: #fff;
   background-color: #6a4029;
   -webkit-box-shadow: 0px 6px 20px rgba(137, 85, 55, 0.4);
-          box-shadow: 0px 6px 20px rgba(137, 85, 55, 0.4);
+  box-shadow: 0px 6px 20px rgba(137, 85, 55, 0.4);
 }
 
 .product-card .set-delivery-time input {
--- src/components/_base/productDetail/ProductDesc.vue
+++ src/components/_base/productDetail/ProductDesc.vue
@@ -1,144 +1,162 @@
 <template>
-    <div>
-        <div class="product-name pl-5 ml-lg-4 poppins">
-            <h1>{{productName.toUpperCase()}}</h1>
-        </div>
-        <div class="product-desc mt-5 poppins">
-            <p>
-                {{productDesc}}
-            </p>
-        </div>
-        <div class="delivery-time poppins mt-5">
-            <p>
-                Delivery only on 
-                <span>
-                <strong>
-                    Monday to friday
-                </strong>
-                    at
-                <strong>{{productStart}} - {{productEnd}}</strong>
-                </span>
-            </p>
-        </div>
-
-        <div class="count-n-price mt-4 mb-4">
-            <b-row>
-                <b-col col lg="6" sm="12">
-                    <div class="product-amount d-flex flex-row">
-                        <div class="p-2">
-                            <button class="btn-min" @click="counterDown()">
-                                -
-                            </button>
-                        </div>
-                        <div class="p-2">
-                            <input type="number" id="subTotal" class="product-total-amount px-2" v-model="productQty">
-                        </div>
-                        <div class="p-2">
-                            <button class="btn-plus" @click="counterUp()">
-                                +
-                            </button>
-                        </div>
-                    </div>
-                </b-col>
-                <b-col col lg="6" sm="12" class="text-right poppins">
-                    <p class="pr-3 mt-2 product-price"><strong>Rp {{subTotal}}</strong></p>
-                </b-col>
-            </b-row>
-        </div>
-        <b-alert
-          :show="dismissCountDown"
-          dismissible
-          variant="success"
-          @dismissed="dismissCountDown=0"
-          @dismiss-count-down="countDownChanged"
-        >
-          <p> {{productName}} added to cart</p>
-          <b-progress
-            variant="success"
-            :max="dismissSecs"
-            :value="dismissCountDown"
-            height="4px"
-          ></b-progress>
-        </b-alert>
-        <div class="btn-add-n-ask poppins">
-        <button @click="addToCart(); showAlert()" class="btn-add mt-3">
-            Add to Cart
-        </button>
-        <br>
-        <button v-if="role === 0" class="btn-ask mt-3">
-            Ask a Staff
-        </button>
-        <br>
-        <router-link :to="{name: 'ProductUpdate', params: {id:productId}}">
-          <button v-if="role === 1" class="btn-ask">
-              Edit Product
-          </button>
-        </router-link>
-        <br>
-        <button v-if="role === 1" @click="deleteProduct(productId)" class="btn-add mt-3" style="background-color: #000;">
-            Delete Product
-        </button>
-        </div>   
+  <div>
+    <div class="product-name pl-5 ml-lg-4 poppins">
+      <h1 class="text-uppercase">{{ productName }}</h1>
     </div>
+    <div class="product-desc mt-5 poppins">
+      <p>
+        {{ productDesc }}
+      </p>
+    </div>
+    <div class="delivery-time poppins mt-5">
+      <p>
+        Delivery only on
+        <span>
+          <strong>
+            Monday to friday
+          </strong>
+          at
+          <strong>{{ productStart }} - {{ productEnd }}</strong>
+        </span>
+      </p>
+    </div>
+
+    <div class="count-n-price mt-4 mb-4">
+      <b-row>
+        <b-col col lg="6" sm="12">
+          <div class="product-amount d-flex flex-row">
+            <div class="p-2">
+              <button class="btn-min" @click="counterDown()">
+                -
+              </button>
+            </div>
+            <div class="p-2">
+              <input
+                type="number"
+                id="subTotal"
+                class="product-total-amount px-2"
+                v-model="productQty"
+              />
+            </div>
+            <div class="p-2">
+              <button class="btn-plus" @click="counterUp()">
+                +
+              </button>
+            </div>
+          </div>
+        </b-col>
+        <b-col col lg="6" sm="12" class="text-right poppins">
+          <p class="pr-3 mt-2 product-price">
+            <strong>Rp {{ subTotal }}</strong>
+          </p>
+        </b-col>
+      </b-row>
+    </div>
+    <b-alert
+      :show="dismissCountDown"
+      dismissible
+      variant="success"
+      @dismissed="dismissCountDown = 0"
+      @dismiss-count-down="countDownChanged"
+    >
+      <p>{{ productName }} added to cart</p>
+      <b-progress
+        variant="success"
+        :max="dismissSecs"
+        :value="dismissCountDown"
+        height="4px"
+      ></b-progress>
+    </b-alert>
+    <div class="btn-add-n-ask poppins">
+      <button
+        @click="
+          addToCart()
+          showAlert()
+        "
+        class="btn-add mt-3"
+      >
+        Add to Cart
+      </button>
+      <br />
+      <button v-if="role === 0" class="btn-ask mt-3">
+        Ask a Staff
+      </button>
+      <br />
+      <router-link :to="{ name: 'ProductUpdate', params: { id: productId } }">
+        <button v-if="role === 1" class="btn-ask">
+          Edit Product
+        </button>
+      </router-link>
+      <br />
+      <button
+        v-if="role === 1"
+        @click="deleteProduct(productId)"
+        class="btn-add mt-3"
+        style="background-color: #000;"
+      >
+        Delete Product
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   props: [
-          'productId',
-          'productName',
-          'productDesc',
-          'productPrice',
-          'productStart',
-          'productEnd',
-          'role'
+    'productId',
+    'productName',
+    'productDesc',
+    'productPrice',
+    'productStart',
+    'productEnd',
+    'role'
   ],
   data() {
-      return {
-          productQty : 0,
-          dismissSecs: 3,
-          dismissCountDown: 0,
-          showDismissibleAlert: false
-      }
+    return {
+      productQty: 0,
+      dismissSecs: 3,
+      dismissCountDown: 0,
+      showDismissibleAlert: false
+    }
   },
   created() {
-      this.counterUp()
-      this.subTotal
+    this.counterUp()
+    this.subTotal
   },
   methods: {
-      deleteProduct(id) {
-        this.$emit('deleteProduct', id)
-      },
-      counterUp() {
-          this.productQty += 1
-          this.getSubTotal(this.subTotal)
-          this.getOrderQty(this.productQty)
-      },
-      counterDown() {
-          this.productQty -= 1
-          this.getSubTotal(this.subTotal)
-          this.getOrderQty(this.productQty)
-      },
-      getOrderQty(qty) {
-          this.$emit('getOrderQty', qty)
-      },
-      getSubTotal(subTotal) {
-          this.$emit('getSubTotal', subTotal)
-      },
-      addToCart(){
-          this.$emit('addToCart')
-      },
-      countDownChanged(dismissCountDown) {
-        this.dismissCountDown = dismissCountDown
-      },
-      showAlert() {
-        this.dismissCountDown = this.dismissSecs
-      }
+    deleteProduct(id) {
+      this.$emit('deleteProduct', id)
+    },
+    counterUp() {
+      this.productQty += 1
+      this.getSubTotal(this.subTotal)
+      this.getOrderQty(this.productQty)
+    },
+    counterDown() {
+      this.productQty -= 1
+      this.getSubTotal(this.subTotal)
+      this.getOrderQty(this.productQty)
+    },
+    getOrderQty(qty) {
+      this.$emit('getOrderQty', qty)
+    },
+    getSubTotal(subTotal) {
+      this.$emit('getSubTotal', subTotal)
+    },
+    addToCart() {
+      this.$emit('addToCart')
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs
+    }
   },
   computed: {
-      subTotal() {
-          return this.productQty * this.productPrice
-      }
+    subTotal() {
+      return this.productQty * this.productPrice
+    }
   }
 }
 </script>
@@ -235,18 +253,18 @@ export default {
   color: #fff;
   background-color: #824e32;
   -webkit-box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
-          box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
 }
 
 .btn-add-n-ask .btn-ask:hover {
   color: #6a4029;
   background-color: #fdc451;
   -webkit-box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
-          box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.34);
 }
 
 .product-price {
-    font-size: 30px;
+  font-size: 30px;
 }
 
 @media (max-width: 576px) {
@@ -266,18 +284,18 @@ export default {
 }
 
 @media (max-width: 375px) {
-    .product-name h1 {
-        font-size: 40px !important;
-    }
+  .product-name h1 {
+    font-size: 40px !important;
+  }
 
-    .btn-add-n-ask .btn-add,
-    .btn-add-n-ask .btn-ask {
-        width: 320px !important;
-        height: 65px;
-        border-radius: 20px;
-        font-size: 20px;
-        font-weight: 700;
-        border: unset;
-    }
+  .btn-add-n-ask .btn-add,
+  .btn-add-n-ask .btn-ask {
+    width: 320px !important;
+    height: 65px;
+    border-radius: 20px;
+    font-size: 20px;
+    font-weight: 700;
+    border: unset;
+  }
 }
 </style>
